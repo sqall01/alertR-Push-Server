3bfab3262b8a21ab819df7207b5aa46ac398d56e
Andre Pawlowski <sqall@towel.hq.h4des.org>
2017-06-14 17:37:37 +0200

    moved to register

---
+++ website/register/index.php
@@ -0,0 +1,12 @@
+<?php
+
+// written by sqall
+// twitter: https://twitter.com/sqall01
+// blog: http://blog.h4des.org
+// github: https://github.com/sqall01
+//
+// Licensed under the GNU Public License, version 2.
+
+require_once("./main.php");
+
+?>
